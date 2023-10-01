e2c1909fb91cb930c9ff5593d7acc0fc8c912889
fawasam <fawasam5@gmail.com>
2023-10-01 21:08:01 +0530

    db changed

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.APP_SECRET = exports.PORT = exports.MONGO_URI = void 0;
-exports.MONGO_URI = "mongodb://localhost:27017/online_foods";
-// export const MONGO_URI =
-//   "mongodb+srv://fawasam:Fawas1234@mern.lscun.mongodb.net/online_foods?retryWrites=true&w=majority";
+// exports.MONGO_URI = "mongodb://localhost:27017/online_foods";
+export const MONGO_URI =
+  "mongodb+srv://fawasam:Fawas1234@mern.lscun.mongodb.net/online_foods?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 8000;
 exports.APP_SECRET = "238745623hsssdf";
 //# sourceMappingURL=index.js.map
